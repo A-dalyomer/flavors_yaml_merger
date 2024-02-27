2c65a93cab806e19dfd09d8b1c513e0cf98bb15e
ahmad daly <Ahmaddo1999@gmail.com>
2024-02-28 03:13:06 +0400

    fixed flavor yaml matcher

--- lib/service/match_flavor_with_main.dart
+++ lib/service/match_flavor_with_main.dart
@@ -2,56 +2,82 @@ import 'dart:io';
 
 import 'package:yaml/yaml.dart';
 
-void matchFlavorFile(
-    {required String flavorYamlPath, required String flavorName}) {
-  /// Load the both working yaml files
-  List<String> mainPubspecLines = File('pubspec.yaml').readAsLinesSync();
-  List<String> flavorPubspecLines = File(flavorYamlPath).readAsLinesSync();
+void matchFlavorFile({
+  required String flavorYamlPath,
+  required String flavorName,
+}) {
+  try {
+    /// Load the both working yaml files
+    List<String> mainPubspecLines = File('pubspec.yaml').readAsLinesSync();
+    List<String> flavorPubspecLines = File(flavorYamlPath).readAsLinesSync();
 
-  /// Load the both working yaml files
-  Map<dynamic, dynamic> flavorPubspec = Map.of(
-    loadYaml(File(flavorYamlPath).readAsStringSync()),
-  );
+    /// Load the both working yaml files
+    Map<dynamic, dynamic> flavorPubspec = Map.of(
+      loadYaml(File(flavorYamlPath).readAsStringSync()),
+    );
 
-  for (int i = 0; i < mainPubspecLines.length; i++) {
-    if (mainPubspecLines[i]
-        .trimLeft()
-        .startsWith("# flavor ${RegExp(r"(?!" + flavorName + r")\b\w+\b")}")) {
-      continue;
-    }
+    bool isInMatching = false;
 
-    /// Skip non flavor lines
-    while (!mainPubspecLines[i].trimLeft().startsWith("# flavor $flavorName")) {
-      if (i + 1 < mainPubspecLines.length) {
-        i++;
-      } else {
-        stdout.writeln("Matched flavor file content");
-        return;
+    for (int i = 0; i < mainPubspecLines.length; i++) {
+      if (mainPubspecLines[i].trim().isEmpty) {
+        continue;
       }
-    }
-    if (i + 1 < mainPubspecLines.length) {
-      i++;
-    }
 
-    bool lineIFound = false;
-    for (int j = 0; j < flavorPubspecLines.length; j++) {
-      bool inNextSection = false;
-      flavorPubspec.forEach((section, value) {
-        if (section == flavorPubspecLines[j].trimLeft().replaceAll('-', '')) {
-          inNextSection = true;
+      print('matching ${mainPubspecLines[i]}');
+
+      if (mainPubspecLines[i]
+          .trimLeft()
+          .startsWith(RegExp(r"^# flavor(?!.*\b" + flavorName + r"\b).*"))) {
+        print('skipped ${mainPubspecLines[i]}');
+        isInMatching = false;
+        continue;
+      }
+
+      /// Skip non flavor lines
+      if (!isInMatching) {
+        while (!mainPubspecLines[i]
+            .trimLeft()
+            .startsWith("# flavor $flavorName")) {
+          if (i + 1 < mainPubspecLines.length) {
+            i++;
+          } else {
+            stdout.writeln("Matched flavor file content");
+            return;
+          }
         }
-      });
+        if (i + 1 < mainPubspecLines.length) {
+          i++;
+        }
+      }
 
-      if (inNextSection) break;
-      if (flavorPubspecLines[j].trimLeft() == mainPubspecLines[i].trimLeft()) {
-        lineIFound = true;
-        break;
+      isInMatching = true;
+      print('in matching ${mainPubspecLines[i]}');
+
+      bool lineIFound = false;
+      for (int j = 0; j < flavorPubspecLines.length; j++) {
+        bool inNextSection = false;
+        flavorPubspec.forEach((section, value) {
+          if (section == flavorPubspecLines[j].trimLeft().replaceAll('-', '')) {
+            inNextSection = true;
+          }
+        });
+
+        if (inNextSection) break;
+        if (flavorPubspecLines[j].trimLeft() ==
+            mainPubspecLines[i].trimLeft()) {
+          print('found line ${flavorPubspecLines[j]}');
+          lineIFound = true;
+          break;
+        }
+      }
+      if (!lineIFound) {
+        stderr.writeln("Missing line in flavor file");
+        stderr.writeln(mainPubspecLines[i]);
+        exit(2);
       }
     }
-    if (!lineIFound) {
-      stderr.writeln("Missing line in flavor file");
-      stderr.writeln(mainPubspecLines[i]);
-      exit(2);
-    }
+  } catch (exception) {
+    stdout.writeln(exception);
+    exit(2);
   }
 }
