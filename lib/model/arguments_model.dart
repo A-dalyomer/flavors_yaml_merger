a8a10252023352e1cb2e11b1bfb7772c8de20f35
ahmad daly <Ahmaddo1999@gmail.com>
2025-08-13 23:52:50 +0300

    Disable auto backup "Can still be enabled from -b"

--- lib/model/arguments_model.dart
+++ lib/model/arguments_model.dart
@@ -29,7 +29,7 @@ class ArgumentsModel {
   factory ArgumentsModel.parse(List<String> arguments) {
     final parser = ArgParser()
       ..addFlag("matcher", abbr: "m", defaultsTo: false)
-      ..addFlag("enable_backup", abbr: "b", defaultsTo: true)
+      ..addFlag("enable_backup", abbr: "b", defaultsTo: false)
       ..addOption("flavor_name", abbr: "f", mandatory: true)
       ..addOption("flavor_path", abbr: "p");
 
